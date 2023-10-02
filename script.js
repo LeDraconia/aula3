b71c803d0f237c46151556e87a279b937ca6d56d
LETICIA EVELYN DE SOUZA LUBRIGATE <leticia.eslubrigate@senacsp.edu.br>
2023-10-02 17:11:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+/*FUNÇÃO COM PARAMETRO
+
+function precos(caneta,lapis){
+    let total = caneta + lapis
+alert("Preço total: "+total.toFixed(2))
+}
+precos(1.50, 2.00)
+
+*/
+
+/*FUNÇÃO SEM PARAMETRO
+
+function digaOla(){
+    alert("Ola")
+}
+
+digaOla()
+
+*/
+
+
+/*
+function multi(a,b){
+    return a * b
+}
+alert(multi (9,10))
+
+*/
+
+/*FUNCAO ANONIMA
+let soma = function(a,b,c){
+    return a+b+c
+}
+
+alert(soma(10,20,30))
+*/
+
+/*
+let subtracao = (a, b) => a - b;
+
+alert(subtracao(100,120))
+*/
+
+/*converter metros em centimetros modelo 1
+
+function conversao (cent){
+    return cent/100
+
+}
+
+alert(conversao(5))
+
+*/
+
+
+/*converter metros em centimetros modelo 2
+
+let conv = (x) => x / 100
+alert(conv(parseFloat(prompt("Digite o valor para converter"))))
+
+*/
+
+/* converter real para dolar
+function converter(real) {
+    return real*5.62
+}
+
+alert(converter(parseFloat(prompt("Digite o valor em reais"))))
+*/
+
+
+
+/* função que receba dois numeros e mostre o maior dele
+
+function conversao(num1,num2){
+
+    if (num1 > num2){
+        alert(num1+" é maior")
+
+    } else {
+        
+        alert(num2+"O numero maior é ")
+
+    }
+
+}
+conversao(100,10)
+
+*/
+
+function converter(f){
+    return (f - 32) * 5/9
+}
+
+alert(converter(20))
+
+alert("Preço total: "+total.toFixed(2))
+
+
+
+
+
+
+
+
+
+
+
+
